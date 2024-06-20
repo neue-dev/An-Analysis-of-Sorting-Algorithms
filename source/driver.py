882746545cefe9b52a3c71b00b2d158949118a6d
MoDavid1964 <modavid.1964@gmail.com>
2024-06-20 16:07:07 +0800

    Final data set 2 + nlogn.

--- source/driver.py
+++ source/driver.py
@@ -7,41 +7,41 @@ import os
 algos = ''  # The algos to test
 out = ''    # The destination
 
-# General test for all algos
-algos = 'insertion,selection,heap,merge,smooth,tim'
-out= 'results/all-n-100-10k-and-p-0-1.csv'
+# # General test for all algos
+# algos = 'insertion,selection,heap,merge,smooth,tim'
+# out= 'results/all-n-100-10k-and-p-0-1.csv'
 
-os.system('./main N=100,250,500,750,1000,2500,5000,7500,10000 P=-0.0000001,-0.1,-0.25,-0.5,-0.75,-0.8,-1.0 cycles=10 runs=10 algos={} out="{}"'.format(algos, out));
-os.system('./main N=100,250,500,750,1000,2500,5000,7500,10000 P=0.0000001,0.1,0.25,0.5,0.75,0.8,1.0 cycles=10 runs=10 algos={} out="{}"'.format(algos, out));
+# os.system('./main N=100,250,500,750,1000,2500,5000,7500,10000 P=-0.0000001,-0.1,-0.25,-0.5,-0.75,-0.8,-1.0 cycles=10 runs=10 algos={} out="{}"'.format(algos, out));
+# os.system('./main N=100,250,500,750,1000,2500,5000,7500,10000 P=0.0000001,0.1,0.25,0.5,0.75,0.8,1.0 cycles=10 runs=10 algos={} out="{}"'.format(algos, out));
 
-# N test for n^2 algorithms
-algos = 'insertion,selection'
-out= 'results/n2-n-test.csv'
+# # N test for n^2 algorithms
+# algos = 'insertion,selection'
+# out= 'results/n2-n-test.csv'
 
-for i in range(1, 10):
-  os.system('./main N={} P=-1.0,-0.667,-0.333,-0.0000001,0.0000001,0.333,0.667,1.0 cycles=10 runs=10 algos={} out="{}"'.format(i * 100, algos, out));
-  os.system('./main N={} P=-1.0,-0.667,-0.333,-0.0000001,0.0000001,0.333,0.667,1.0 cycles=10 runs=10 algos={} out="{}"'.format(i * 1000, algos, out));
+# for i in range(1, 10):
+#   os.system('./main N={} P=-1.0,-0.667,-0.333,-0.0000001,0.0000001,0.333,0.667,1.0 cycles=10 runs=10 algos={} out="{}"'.format(i * 100, algos, out));
+#   os.system('./main N={} P=-1.0,-0.667,-0.333,-0.0000001,0.0000001,0.333,0.667,1.0 cycles=10 runs=10 algos={} out="{}"'.format(i * 1000, algos, out));
 
-os.system('./main N=10000 P=-1.0,-0.667,-0.333,-0.0000001,0.0000001,0.333,0.667,1.0 cycles=10 runs=10 algos={} out="{}"'.format(algos, out));
+# os.system('./main N=10000 P=-1.0,-0.667,-0.333,-0.0000001,0.0000001,0.333,0.667,1.0 cycles=10 runs=10 algos={} out="{}"'.format(algos, out));
 
-# P test for n^2 algorithms
-algos = 'insertion,selection'
-out= 'results/n2-p-test.csv'
+# # P test for n^2 algorithms
+# algos = 'insertion,selection'
+# out= 'results/n2-p-test.csv'
 
-os.system('./main N=100,500,1000,5000,10000 P=-0.0000001 cycles=10 runs=10 algos={} out="{}"'.format(algos, out));
-os.system('./main N=100,500,1000,5000,10000 P=0.0000001 cycles=10 runs=10 algos={} out="{}"'.format(algos, out));
+# os.system('./main N=100,500,1000,5000,10000 P=-0.0000001 cycles=10 runs=10 algos={} out="{}"'.format(algos, out));
+# os.system('./main N=100,500,1000,5000,10000 P=0.0000001 cycles=10 runs=10 algos={} out="{}"'.format(algos, out));
 
-for i in range(-9, 10):
-  if i != 0:
-    os.system('./main N=100,500,1000,5000,10000 P={} cycles=10 runs=10 algos={} out="{}"'.format(i * 0.01, algos, out));
-    os.system('./main N=100,500,1000,5000,10000 P={} cycles=10 runs=10 algos={} out="{}"'.format(i * 0.1, algos, out));
+# for i in range(-9, 10):
+#   if i != 0:
+#     os.system('./main N=100,500,1000,5000,10000 P={} cycles=10 runs=10 algos={} out="{}"'.format(i * 0.01, algos, out));
+#     os.system('./main N=100,500,1000,5000,10000 P={} cycles=10 runs=10 algos={} out="{}"'.format(i * 0.1, algos, out));
 
-os.system('./main N=100,500,1000,5000,10000 P=-1.0 cycles=10 runs=10 algos={} out="{}"'.format(algos, out));
-os.system('./main N=100,500,1000,5000,10000 P=1.0 cycles=10 runs=10 algos={} out="{}"'.format(algos, out));
+# os.system('./main N=100,500,1000,5000,10000 P=-1.0 cycles=10 runs=10 algos={} out="{}"'.format(algos, out));
+# os.system('./main N=100,500,1000,5000,10000 P=1.0 cycles=10 runs=10 algos={} out="{}"'.format(algos, out));
 
 # N test for nlogn algorithms
 algos = 'heap,merge,smooth,tim'
-out = 'results/results/nlogn-n-test.csv'
+out = 'results/nlogn-n-test.csv'
 
 os.system('./main N=1000 P=-1.0,-0.667,-0.333,-0.0000001,0.0000001,0.333,0.667,1.0 cycles=10 runs=10 algos={} out="{}"'.format(algos, out));
 os.system('./main N=2000 P=-1.0,-0.667,-0.333,-0.0000001,0.0000001,0.333,0.667,1.0 cycles=10 runs=10 algos={} out="{}"'.format(algos, out));
@@ -107,7 +107,7 @@ os.system('./main N=100000 P=1.0 cycles=10 runs=10 algos={} out="{}"'.format(alg
 
 # P test for nlogn algorithms
 algos = 'heap,merge,smooth,tim'
-out = 'results/results/nlogn-p-test.csv'
+out = 'results/nlogn-p-test.csv'
 
 os.system('./main N=1000,2500,5000,10000,15000,20000,25000 P=-0.0000001 cycles=10 runs=10 algos={} out="{}"'.format(algos, out));
 os.system('./main N=1000,2500,5000,10000,15000,20000,25000 P=0.0000001 cycles=10 runs=10 algos={} out="{}"'.format(algos, out));
@@ -120,13 +120,13 @@ for i in range(-9, 10):
 os.system('./main N=1000,2500,5000,10000,15000,20000,25000 P=-1.0 cycles=10 runs=10 algos={} out="{}"'.format(algos, out));
 os.system('./main N=1000,2500,5000,10000,15000,20000,25000 P=1.0 cycles=10 runs=10 algos={} out="{}"'.format(algos, out));
 
-# Smooth sort vs heap sort
-algos = 'heap,smooth'
-out = 'results/smooth-heap-p-test.csv'
+# # Smooth sort vs heap sort
+# algos = 'heap,smooth'
+# out = 'results/smooth-heap-p-test.csv'
 
-os.system('./main N=100000 P=-0.0000001 cycles=10 runs=10 algos={} out="{}"'.format(algos, out));
-os.system('./main N=100000 P=0.0000001 cycles=10 runs=10 algos={} out="{}"'.format(algos, out));
+# os.system('./main N=100000 P=-0.0000001 cycles=10 runs=10 algos={} out="{}"'.format(algos, out));
+# os.system('./main N=100000 P=0.0000001 cycles=10 runs=10 algos={} out="{}"'.format(algos, out));
 
-for i in range(-100, 101):
-  if i != 0:
-    os.system('./main N=100000 P={} cycles=10 runs=10 algos={} out="{}"'.format(i / 100, algos, out));
+# for i in range(-100, 101):
+#   if i != 0:
+#     os.system('./main N=100000 P={} cycles=10 runs=10 algos={} out="{}"'.format(i / 100, algos, out));
